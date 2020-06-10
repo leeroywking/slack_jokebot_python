cd7a3525015cc23ec16b20edffc49b1f2bec6398
Lee-Roy King <leeroywking@gmail.com>
2020-06-09 19:53:23 -0700

    first try

--- app.py
+++ app.py
@@ -9,7 +9,12 @@ app = Flask(__name__)
 def joke():
     res = requests.get('https://icanhazdadjoke.com/',{},headers={'Accept':'text/plain'})
     print(res.content)
+    dataobj = {"text":res.content}
+    requests.post('https://hooks.slack.com/services/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', data=dataobj)
+    
     return res.content
 
 if __name__ == '__main__':
     app.run()
+
+
